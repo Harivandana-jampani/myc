73a97ca83adde38049c306e1c259610fe1423152
vandana <harivandana.jampani@gmail.com>
2023-08-02 20:00:35 +0530

    p39

---
+++ p39.c
@@ -0,0 +1,27 @@
+/* 
+*
+**
+* *
+*  *
+*****
+*/
+#include <stdio.h>
+
+int main() {
+int i,j;
+for(i=1;i<=5;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        if(i==1||i==5||i==j||j==1)
+        {
+            printf("*");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+        printf("\n");
+    }
+}
