c1e29a66ebb272ad636280a886931d577f1f1611
vandana <harivandana.jampani@gmail.com>
2023-08-03 22:33:54 +0530

    p48

--- p48.c
+++ p48.c
@@ -1,20 +1,20 @@
-/*   1 
-    0 -1 
-   -2 -3 -4 
-  -5 -6 -7 -8 
- -9 -10 -11 -12 -13 
+/* 1 2 3 4 5 
+  1 2 3 4 
+   1 2 3 
+    1 2 
+     1 
 */
 #include <stdio.h>
 
 int main() {
 int i,j,k,c=1;
-for(i=1;i<=5;i++)
+for(i=5;i>=1;i--)
 {
     for(k=5;k>=i;k--)
         printf(" ");
     for(j=1;j<=i;j++)
     {
-        printf("%d ",c--);
+        printf("%d ",j);
     }
         printf("\n");
     
