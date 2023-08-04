6107100eda068088f1e747ca06edd661d13fc506
vandana <harivandana.jampani@gmail.com>
2023-08-04 19:11:53 +0530

    p54

---
+++ p53.c
@@ -0,0 +1,24 @@
+/* 
+     A 
+    B B 
+   C C C 
+  D D D D 
+ E E E E E */
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(k=5;k>=i;k--)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%c ",i+64);
+        }
+        printf("\n");
+    }
+}
---
+++ p54.c
@@ -0,0 +1,24 @@
+/*   A 
+    B A 
+   C B A 
+  D C B A 
+ E D C B A 
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(k=5;k>=i;k--)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%c ",j+64);
+        }
+        printf("\n");
+    }
+}
